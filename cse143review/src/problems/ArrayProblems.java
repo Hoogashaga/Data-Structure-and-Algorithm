ac3a2091c8d7c280cd5c8d6c501e9493bc7c70ff
shchen77 <shchen77@uw.edu>
2021-04-06 20:31:03 -0700

    Array and Map

--- cse143review/src/problems/ArrayProblems.java
+++ cse143review/src/problems/ArrayProblems.java
@@ -13,18 +13,42 @@ public class ArrayProblems {
      * Returns a `String` representation of the input array.
      * Always starts with '[' and ends with ']'; elements are separated by ',' and a space.
      */
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static String toString(int[] array) {
         // TODO replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (array.length == 0){
+            return "[]";
+        }
+
+        int len = array.length;
+        String newString = "[";
+        for (int index = 0; index < len - 1; index++) {
+            newString = newString + array[index] + ", ";
+        }
+        newString = newString + array[len - 1] + "]";
+        return newString;
+
+        // throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     /**
      * Returns a new array containing the input array's elements in reversed order.
      * Does not modify the input array.
      */
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static int[] reverse(int[] array) {
         // TODO replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        int i = 0;
+        int j = array.length - 1;
+        int[] reversedArray = new int[j+1];
+
+        for (int index = j; 0 <= index; index--){
+            reversedArray[i] = array[index];
+            i += 1;
+        }
+
+        return reversedArray;
+        // throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     /**
@@ -32,6 +56,19 @@ public class ArrayProblems {
      */
     public static void rotateRight(int[] array) {
         // TODO replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if(array.length == 0){
+            array = array;
+        }
+        else{
+            int j = array.length - 1;
+            int tmp = array[j];
+            for (int index = j; index > 0; index--){
+                array[index] = array[index-1];
+            }
+            array[0] = tmp;
+        }
+
+        // return array;
+        // throw new UnsupportedOperationException("Not implemented yet.");
     }
 }
--- cse143review/src/problems/MapProblems.java
+++ cse143review/src/problems/MapProblems.java
@@ -2,6 +2,7 @@ package problems;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * See the spec on the website for example behavior.
@@ -11,9 +12,26 @@ public class MapProblems {
     /**
      * Returns true if any string appears at least 3 times in the given list; false otherwise.
      */
+    @SuppressWarnings("checkstyle:WhitespaceAfter")
     public static boolean contains3(List<String> list) {
         // TODO replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for(int i = 0; i < list.size(); i++){
+            if(map.containsKey(list.get(i))) {
+                int value = map.get(list.get(i)) + 1;
+                map.put(list.get(i), value);
+                if (map.get(list.get(i)) >= 3) {
+                    return true;
+                }
+            }
+            else{
+                map.put(list.get(i), 1);
+            }
+        }
+        return false;
+
+
+        // throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     /**
@@ -22,6 +40,16 @@ public class MapProblems {
      */
     public static Map<String, Integer> intersect(Map<String, Integer> m1, Map<String, Integer> m2) {
         // TODO replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Map<String, Integer> m3 = new HashMap<String, Integer>();
+        for (String key : m1.keySet()) {
+            if (m2.containsKey(key)) {
+                if(m1.get(key) == m2.get(key)) {
+                    m3.put(key, m2.get(key));
+                }
+            }
+        }
+        return m3;
+
+        // throw new UnsupportedOperationException("Not implemented yet.");
     }
 }
